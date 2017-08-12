d22e202979d412787ad563a384d163ef56831f5b
Frank Felhoffer <silveraid@hackme.ca>
2017-08-12 19:00:44 -0400

    Added bootstrap menu

--- functions.php
+++ functions.php
@@ -173,3 +173,9 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+/**
+ *.Load Bootstrap Menu.
+ */
+require get_template_directory() . '/inc/bootstrap-walker.php';
+
