df652abb0e41972dad3540c9c3ed7ed42f5bd0e8
Frank Felhoffer <silveraid@hackme.ca>
2017-08-12 19:03:36 -0400

    Removed skip-content link

--- header.php
+++ header.php
@@ -22,8 +22,6 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'theme-slug' ); ?></a>
-
 	<header id="masthead" class="site-header">
 		<nav role="navigation">
 			<div class="navbar navbar-static-top navbar-default">
