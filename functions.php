1faf721f30f9b0ff288300a8afd3ff5a5157a233
Frank Felhoffer <silveraid@hackme.ca>
2017-08-12 14:05:47 -0400

    Added the respond.js to support old browsers

--- functions.php
+++ functions.php
@@ -132,6 +132,20 @@ function theme_slug_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_slug_scripts' );
 
+if (!function_exists('ie_scripts')) {
+
+	function ie_scripts() {
+		echo '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->';
+	    echo '<!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->';
+	    echo '<!--[if lt IE 9]>';
+	    echo '  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>';
+	    echo '  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
+	    echo '<![endif]-->';
+	}
+
+    add_action('wp_head', 'ie_scripts');
+}
+
 /**
  * Implement the Custom Header feature.
  */
