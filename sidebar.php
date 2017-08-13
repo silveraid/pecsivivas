3f6e6f901492280932947cb2f1af09d86139c476
Frank Felhoffer <silveraid@hackme.ca>
2017-08-13 13:49:14 -0400

    Changed the size of the widget titles, changed the formatting of the sidebar

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area col-md-3 col-lg-3">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<div class="well">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
 </aside><!-- #secondary -->
 
